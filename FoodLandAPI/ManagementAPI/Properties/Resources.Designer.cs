ca4d9ef661b6c5654b70b5564a48e71c19d456d1
Jabavokza <jabavok.007@gmail.com>
2019-01-10 14:52:10 +0700

    Initial commit

--- FoodLandAPI/ManagementAPI/Properties/Resources.Designer.cs
+++ FoodLandAPI/ManagementAPI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ManagementAPI.Properties {
+namespace AdaFLCouponManagementAPI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ManagementAPI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ManagementAPI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AdaFLCouponManagementAPI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
